ae14fb0891c90a1db316e4c0707d95d148ae769b
Anish Gupta <anish.gupta05497@gmail.com>
2023-07-16 14:10:35 +0530

    Update Food.js

--- src/components/Food.js
+++ src/components/Food.js
@@ -1,10 +1,21 @@
-import React from 'react';
+// import React from 'react';
 
-export default (props) => {
+// export default (props) => {
 
  
 
-  return (
-    <div className="snake-food"></div>
-  )
-}
+//   return (
+//     <div className="snake-food"></div>
+//   )
+// }
+
+import React from "react";
+
+export default (props) => {
+  const style = {
+    left: `${props.dot[0]}%`,
+    top: `${props.dot[1]}%`
+  };
+
+  return <div className="snake-food" style={style}></div>;
+};
